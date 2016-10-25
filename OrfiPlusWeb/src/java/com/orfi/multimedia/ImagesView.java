786e39480b9f4354420745fc0378f4b7c806686f
Sergio Portatil <savillarraga@misena.edu.co>
2016-10-25 03:37:12 -0500

    Se eliminaron cosas que estaban duplicadas o que no tenia que ver con orfi se añadieron nuevos estilos en tablas y formularios en admin favor mirar cada pagina de admin para comprender mas.

---
+++ OrfiPlusWeb/src/java/com/orfi/multimedia/ImagesView.java
@@ -0,0 +1,23 @@
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class ImagesView {
+
+    private List<String> images;
+
+    @PostConstruct
+    public void init() {
+        images = new ArrayList<String>();
+        for (int i = 1; i <= 4; i++) {
+            images.add("inspiracion" + i + ".jpg");
+        }
+    }
+
+    public List<String> getImages() {
+        return images;
+    }
+}
